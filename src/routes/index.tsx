94b988f4889d592c58fec08ccc263947247f7e0b
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2021-06-25 11:31:08 -0300

    app finalizado

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,8 +10,8 @@ export const Routes = () => {
     <Switch>
       <Route path='/' exact component={Home}/>
       <Route path='/rooms/new' exact component={NewRoom}/>
-      <Route path='/rooms/:id' component={Room}/>
-      <Route path='/admin/rooms/:id' component={AdminRoom}/>
+      <Route path='/rooms/:id' exact component={Room}/>
+      <Route path='/rooms/admin/:id' exact component={AdminRoom}/>
     </Switch>
   )
 }
